a5edebb7c919b8ce7c68d8dff59e383aa022eb2e
Abai.Sul <abai.sultanov79@gmail.com>
2023-01-12 00:04:29 +0600

    php password fixed

--- src/mailer/smart.php
+++ src/mailer/smart.php
@@ -16,7 +16,7 @@ $mail->isSMTP();                                      // Set mailer to use SMTP
 $mail->Host = 'smtp.mail.ru';  // Specify main and backup SMTP servers
 $mail->SMTPAuth = true;                               // Enable SMTP authentication
 $mail->Username = 'boxforyoukg@mail.ru';        // Наш логин
-$mail->Password = 'aba1100806@';                         // Наш пароль от ящика
+$mail->Password = 'fzrpACgMHhCH9g1nfMkU';                         // Наш пароль от ящика
 $mail->SMTPSecure = 'ssl';                            // Enable TLS encryption, `ssl` also accepted
 $mail->Port = 465;                                    // TCP port to connect to
  
